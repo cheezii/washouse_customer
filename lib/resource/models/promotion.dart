c276f9c6d2dd7a700c3331b327ca4eb712b63e6b
cheezii <chipnquynh@gmail.com>
2023-04-05 16:05:46 +0700

    promotion for cart

--- lib/resource/models/promotion.dart
+++ lib/resource/models/promotion.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-class Promotion {
+class PromotionModel {
   int id;
   String code;
   String expiredDate;
   String description;
-  Promotion({
+  PromotionModel({
     required this.id,
     required this.code,
     required this.expiredDate,
@@ -12,20 +12,20 @@ class Promotion {
   });
 }
 
-List<Promotion> demoPromotionList = [
-  Promotion(
+List<PromotionModel> demoPromotionList = [
+  PromotionModel(
     id: 1,
     code: 'TEST_DRCLEAN_FAIL',
     expiredDate: '2023-06-30',
     description: 'Sử dụng trong tháng 6',
   ),
-  Promotion(
+  PromotionModel(
     id: 1,
     code: 'TEST_DRCLEAN_OK',
     expiredDate: '2023-04-25',
     description: 'Sử dụng đến hết ngày 20/4/2023',
   ),
-  Promotion(
+  PromotionModel(
     id: 1,
     code: 'TEST_DRCLEAN_FAIL',
     expiredDate: '2023-04-02',
