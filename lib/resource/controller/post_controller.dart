3e861fb85e8d14a0a92811e43f094f92a8dbf319
datlt2001 <thanhdat3001@gmail.com>
2023-04-17 13:25:37 +0700

    fetch api posts list

--- lib/resource/controller/post_controller.dart
+++ lib/resource/controller/post_controller.dart
@@ -3,27 +3,29 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 import 'package:washouse_customer/resource/controller/base_controller.dart';
-import 'package:washouse_customer/resource/models/response_models/notification_item_response.dart';
 import '../../components/constants/text_constants.dart';
+import '../models/post.dart';
 
 BaseController baseController = BaseController();
 
-// class PostController {
-//   Future<P> getNotifications() async {
-//     NotificationResponse notificationResponse = NotificationResponse();
-//     try {
-//       String url = '$baseUrl/notifications/me-noti';
-//       Response response = await baseController.makeAuthenticatedRequest(url, {});
-
-//       if (response.statusCode == 200) {
-//         var data = jsonDecode(response.body)["data"];
-//         notificationResponse = NotificationResponse.fromJson(data);
-//       } else {
-//         throw Exception('Error fetching getNotifications: ${response.statusCode}');
-//       }
-//     } catch (e) {
-//       print('error: getNotifications-$e');
-//     }
-//     return notificationResponse;
-//   }
-// }
+class PostController {
+  Future<List<Post>> getPosts() async {
+    List<Post> posts = [];
+    try {
+      String url = '$baseUrl/posts';
+      final Uri uri = Uri.parse(url);
+      final Map<String, String> queryParams = {};
+      final response = await http.get(uri.replace(queryParameters: queryParams));
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body)['data']['items'] as List;
+        // Handle successful response
+        posts = data.map((e) => Post.fromJson(e)).toList();
+      } else {
+        throw Exception('Error fetching getPosts: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('error: getPosts-$e');
+    }
+    return posts;
+  }
+}
